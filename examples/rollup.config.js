f2aba4d243d68645c55f2253b27321c8bedef2dc
Jeshurun Hembd <jjhembd@gmail.com>
2019-10-28 11:18:13 -0500

    Automate examples build script

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,19 +1,28 @@
+var fs = require('fs');
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs'; // Yuck... avoid if possible
 import json from 'rollup-plugin-json';
 
-export default [
-//  {
-//    input: '',
-//    plugins: [
-//      resolve(),
-//      commonjs(),
-//      json(),
-//    ],
-//    output: {
-//      file: 'main.min.js',
-//      format: 'iife',
-//      name: 'example',
-//    },
-//  }
-];
+// Get a list of the directory names
+const dirNames = fs
+  .readdirSync('./', { withFileTypes: true })
+  .filter(d => d.isDirectory())
+  .map(d => d.name);
+
+// Function to make a rollup config object from a directory name
+function makeConfig(dir) {
+  return {
+    input: dir + '/main.js',
+    plugins: [
+      json(),
+      resolve(),
+    ],
+    output: {
+      file: dir + '/main.min.js',
+      format: 'iife',
+      name: 'app',
+    }
+  };
+}
+
+// Export an array of config objects
+export default dirNames.map(makeConfig);
